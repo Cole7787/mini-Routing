17304be9d65fdccd6f7a909317bced60d17253fa
Cole Allan <cacoleallan@gmail.com>
2016-12-19 13:55:15 -0700

    Completed mini routing project

---
+++ js/app.js
@@ -0,0 +1,21 @@
+angular.module('miniRouting', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: 'js/home/home.html',
+    controller: 'homeCtrl'
+  })
+  .state('settings', {
+    url: '/settings',
+    templateUrl: 'js/settings/settings.html',
+    controller: 'settingsCtrl'
+  })
+  .state('products', {
+    url: '/products/:id',
+    templateUrl: 'js/products/products.html',
+    controller: 'productsCtrl'
+  })
+  $urlRouterProvider
+  .otherwise('/');
+});
